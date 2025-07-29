fcaf0f54f67371a27cc0608da0f4261c44ee87b7
Kaaikani <135017128+kaaikani@users.noreply.github.com>
2025-07-29 12:27:47 +0530

    Update vendure-config.ts

--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -46,7 +46,7 @@ const IS_DEV = process.env.APP_ENV === 'dev';
 export const config: VendureConfig = {
   // logger: new DefaultLogger({ level: LogLevel.Verbose }),
   apiOptions: {
-    port: 3000,
+    port: 80,
     adminApiPath: 'admin-api',
     shopApiPath: 'shop-api',
     adminListQueryLimit:2000,
@@ -197,7 +197,7 @@ export const config: VendureConfig = {
       },
     }),
     AdminUiPlugin.init({
-      port: 3000,
+      port: 8080,
       app: {
         path: path.join(__dirname, '../admin-ui/dist'),
       },
